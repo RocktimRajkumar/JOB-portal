2b0b77a1adf7ad1e5b628d8abe692eb01eb185f6
Rocktim <rocktim53@gmail.com>
2018-03-25 19:10:57 +0530

    initial add

---
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
@@ -0,0 +1,13 @@
+package brdevelopers.com.jobvibe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
