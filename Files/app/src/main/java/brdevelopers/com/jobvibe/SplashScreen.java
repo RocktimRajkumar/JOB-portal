95e2cb9b5c2500d11f5dbe13ea49f27224b78908
Rocktim <rocktim53@gmail.com>
2018-03-27 15:11:54 +0530

    profile_education added, Current Education design done

--- Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
@@ -8,6 +8,6 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.profile_education);
     }
 }
