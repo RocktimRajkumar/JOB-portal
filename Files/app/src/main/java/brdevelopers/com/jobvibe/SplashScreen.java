64cb66cc0b4216ec15a9068bb354015119604b7f
BijoyDutta95 <bijoy10dutta@gmail.com>
2018-03-27 19:47:21 +0530

    profile_eduction and personal design modified final

--- Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
@@ -8,6 +8,6 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_personal);
+        setContentView(R.layout.profile_education);
     }
 }
