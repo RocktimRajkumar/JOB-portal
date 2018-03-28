ba046e1c5b96b25edf0e3a4aaa541aa1abf1352e
Rocktim <rocktim53@gmail.com>
2018-03-28 13:18:37 +0530

    signup (xml, Java) modified

--- Files/app/src/main/java/brdevelopers/com/jobvibe/Sign_up.java
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/Sign_up.java
@@ -3,26 +3,58 @@ package brdevelopers.com.jobvibe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
  * Created by End User on 27-03-2018.
  */
 
-public class Sign_up extends AppCompatActivity{
+public class Sign_up extends AppCompatActivity implements TextWatcher{
 
+    TextInputLayout til_password,til_cpassword;
+    EditText et_password,et_cpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        til_password=findViewById(R.id.TIL_password);
+        til_cpassword=findViewById(R.id.TIL_cpassword);
+        et_password=findViewById(R.id.ET_password);
+        et_cpassword=findViewById(R.id.ET_cpassword);
     }
 
     public void openProfile(View view)
     {
         Intent profile=new Intent(Sign_up.this,Profile.class);
         startActivity(profile);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+        if(et_password.getText().length()>0){
+
+        }
+        else{
+
+        }
     }
 }
