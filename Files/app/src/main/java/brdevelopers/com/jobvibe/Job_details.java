d426e94f2204d88b7bcca1227b149fab1f654095
Rocktim <rocktim53@gmail.com>
2018-04-01 19:06:42 +0530

    job_details class added

---
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/Job_details.java
@@ -0,0 +1,249 @@
+package brdevelopers.com.jobvibe;
+
+public class Job_details {
+
+    private String jbid=null;
+    private String jbtitle=null;
+    private String jblocation=null;
+    private String jbldapply=null;
+    private String jbcompnayname=null;
+    private String jburl=null;
+    private String jbsalary=null;
+    private String jbbca=null;
+    private String jbmca=null;
+    private String jbcse=null;
+    private String jbit=null;
+    private String jbee=null;
+    private String jbece=null;
+    private String jbcivil=null;
+    private String jbmba=null;
+    private String jbasp=null;
+    private String jbphp=null;
+    private String jbjava=null;
+    private String jbios=null;
+    private String jbandroid=null;
+    private String jbdbms=null;
+    private String jbwrittentest=null;
+    private String jbwalking=null;
+    private String jbonline=null;
+    private String jbdescription=null;
+    private String jbcompanyprofile=null;
+    private String jbemail=null;
+
+    public String getJbid() {
+        return jbid;
+    }
+
+    public void setJbid(String jbid) {
+        this.jbid = jbid;
+    }
+
+    public String getJbtitle() {
+        return jbtitle;
+    }
+
+    public void setJbtitle(String jbtitle) {
+        this.jbtitle = jbtitle;
+    }
+
+    public String getJblocation() {
+        return jblocation;
+    }
+
+    public void setJblocation(String jblocation) {
+        this.jblocation = jblocation;
+    }
+
+    public String getJbldapply() {
+        return jbldapply;
+    }
+
+    public void setJbldapply(String jbldapply) {
+        this.jbldapply = jbldapply;
+    }
+
+    public String getJbcompnayname() {
+        return jbcompnayname;
+    }
+
+    public void setJbcompnayname(String jbcompnayname) {
+        this.jbcompnayname = jbcompnayname;
+    }
+
+    public String getJburl() {
+        return jburl;
+    }
+
+    public void setJburl(String jburl) {
+        this.jburl = jburl;
+    }
+
+    public String getJbsalary() {
+        return jbsalary;
+    }
+
+    public void setJbsalary(String jbsalary) {
+        this.jbsalary = jbsalary;
+    }
+
+    public String getJbbca() {
+        return jbbca;
+    }
+
+    public void setJbbca(String jbbca) {
+        this.jbbca = jbbca;
+    }
+
+    public String getJbmca() {
+        return jbmca;
+    }
+
+    public void setJbmca(String jbmca) {
+        this.jbmca = jbmca;
+    }
+
+    public String getJbcse() {
+        return jbcse;
+    }
+
+    public void setJbcse(String jbcse) {
+        this.jbcse = jbcse;
+    }
+
+    public String getJbit() {
+        return jbit;
+    }
+
+    public void setJbit(String jbit) {
+        this.jbit = jbit;
+    }
+
+    public String getJbee() {
+        return jbee;
+    }
+
+    public void setJbee(String jbee) {
+        this.jbee = jbee;
+    }
+
+    public String getJbece() {
+        return jbece;
+    }
+
+    public void setJbece(String jbece) {
+        this.jbece = jbece;
+    }
+
+    public String getJbcivil() {
+        return jbcivil;
+    }
+
+    public void setJbcivil(String jbcivil) {
+        this.jbcivil = jbcivil;
+    }
+
+    public String getJbmba() {
+        return jbmba;
+    }
+
+    public void setJbmba(String jbmba) {
+        this.jbmba = jbmba;
+    }
+
+    public String getJbasp() {
+        return jbasp;
+    }
+
+    public void setJbasp(String jbasp) {
+        this.jbasp = jbasp;
+    }
+
+    public String getJbphp() {
+        return jbphp;
+    }
+
+    public void setJbphp(String jbphp) {
+        this.jbphp = jbphp;
+    }
+
+    public String getJbjava() {
+        return jbjava;
+    }
+
+    public void setJbjava(String jbjava) {
+        this.jbjava = jbjava;
+    }
+
+    public String getJbios() {
+        return jbios;
+    }
+
+    public void setJbios(String jbios) {
+        this.jbios = jbios;
+    }
+
+    public String getJbandroid() {
+        return jbandroid;
+    }
+
+    public void setJbandroid(String jbandroid) {
+        this.jbandroid = jbandroid;
+    }
+
+    public String getJbdbms() {
+        return jbdbms;
+    }
+
+    public void setJbdbms(String jbdbms) {
+        this.jbdbms = jbdbms;
+    }
+
+    public String getJbwrittentest() {
+        return jbwrittentest;
+    }
+
+    public void setJbwrittentest(String jbwrittentest) {
+        this.jbwrittentest = jbwrittentest;
+    }
+
+    public String getJbwalking() {
+        return jbwalking;
+    }
+
+    public void setJbwalking(String jbwalking) {
+        this.jbwalking = jbwalking;
+    }
+
+    public String getJbonline() {
+        return jbonline;
+    }
+
+    public void setJbonline(String jbonline) {
+        this.jbonline = jbonline;
+    }
+
+    public String getJbdescription() {
+        return jbdescription;
+    }
+
+    public void setJbdescription(String jbdescription) {
+        this.jbdescription = jbdescription;
+    }
+
+    public String getJbcompanyprofile() {
+        return jbcompanyprofile;
+    }
+
+    public void setJbcompanyprofile(String jbcompanyprofile) {
+        this.jbcompanyprofile = jbcompanyprofile;
+    }
+
+    public String getJbemail() {
+        return jbemail;
+    }
+
+    public void setJbemail(String jbemail) {
+        this.jbemail = jbemail;
+    }
+
+}
