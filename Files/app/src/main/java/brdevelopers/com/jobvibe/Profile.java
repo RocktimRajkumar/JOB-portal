37aaf9caf8416d6a28c5e387eeaf23db1da9d1a4
BijoyDutta95 <bijoy10dutta@gmail.com>
2018-04-17 16:00:43 +0530

    backpress added on advance information,content home modifiy

--- Files/app/src/main/java/brdevelopers/com/jobvibe/Profile.java
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/Profile.java
@@ -7,8 +7,13 @@ import android.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,6 +22,7 @@ import android.widget.TextView;
 public class Profile extends AppCompatActivity implements View.OnClickListener{
 
     public static TextView tv_personal,tv_education;
+    private boolean onbackpressed=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +60,33 @@ public class Profile extends AppCompatActivity implements View.OnClickListener{
         }
 
     }
+
+    @Override
+    public void onBackPressed() {
+       if(!onbackpressed){
+
+            View v=findViewById(android.R.id.content);
+            Snackbar.make(v,"Press back again to exit "+ Html.fromHtml("&#9995;"),Snackbar.LENGTH_SHORT).show();
+
+            onbackpressed=true;
+        }
+        else{
+            super.onBackPressed();
+        }
+        new CountDownTimer(3000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                onbackpressed=false;
+            }
+        }.start();
+    }
+
 
     private void loadFragmentProfile(Fragment fragment) {
 
