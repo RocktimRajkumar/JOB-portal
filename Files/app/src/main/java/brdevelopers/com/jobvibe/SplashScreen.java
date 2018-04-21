8a8040a21b9bfaf9b9d3a231d17e5dc1addc3038
Rocktim <rocktim53@gmail.com>
2018-04-21 13:33:42 +0530

    Splash Screen FLag Full Screen done

--- Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
+++ Files/app/src/main/java/brdevelopers/com/jobvibe/SplashScreen.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,6 +16,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(new Runnable() {
             @Override
